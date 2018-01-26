affbf24946928ed34b16d967512a6ec93cd5a8ec
Mega <mega@github.com>
2018-01-26 14:32:02 -0800

    first commit

---
+++ lab3.py
@@ -0,0 +1,13 @@
+# lab 3: GUI with Tkinter, OOP
+# Name: Mega Putra 
+
+# class AddStudentDialog
+    
+# class MainWindow
+ 
+        
+def main() :
+    win = MainWindow()
+    win.mainloop()
+
+main()

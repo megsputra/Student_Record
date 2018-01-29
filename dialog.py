01740b3b30be4b9ab29e299a6621673d6b0da682
Mega <mega@github.com>
2018-01-29 13:52:05 -0800

    test pushing after resolve conflict

--- dialog.py
+++ dialog.py
@@ -1,26 +1,4 @@
 # CIS 41B
-<<<<<<< HEAD
-# Name: Mega Putra 
-# Dialog window base class
-
-#
-
-
-import tkinter as tk
-from abc import ABC, abstractmethod # abstact base class, a class to derive other classes from
-# should never be instantiated because you dont have a body for it, empty window with buttons
-# framework should be made as an abstract class
-# why is MultipleInheritance bad? When/Why is this used? this is why!!!
-
-class Dialog(tk.Toplevel, ABC):    # Multiple inheritance: Dialog gets the attributes of TopLevel and ABC
-    """Base Dialog() class
-       - Has standard features of a dialog box: 
-          - window with [OK] and [Cancel] buttons
-          - [OK] to commit a transaction, [Cancel] to cancel a transaction
-       - Derived dialog boxes can be created with a small amount of customization"""
-
-    def __init__(self, master, title=None, **kwargs):    # Q1. what is kwargs and why would this base class want to use it
-=======
 # Dialog window base class
 
 import tkinter as tk
@@ -39,7 +17,6 @@ class Dialog(tk.Toplevel, ABC):    # Multiple inheritance: Dialog gets the attri
                                                          # kwargs means Key Word Arguemnts
                                                          # **kwargs is for unpacking keywords argument into function definition
                                                          # later on, it can be used as: for k, v in kwargs : dosomething()
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
         """ set up window with title, body, [OK] and [Cancel] buttons, and controls"""
         
         ABC.__init__(self)
@@ -47,26 +24,6 @@ class Dialog(tk.Toplevel, ABC):    # Multiple inheritance: Dialog gets the attri
                                             # master is window that Dialog is spawned from, which in lab 3 is MainWindow
                                             # master needs to be passed in so that if master closes, then all spawned windows will go away
 
-<<<<<<< HEAD
-        self.grab_set()  #gives focus to a window when you have multiple windows.
-        # dialog window
-        # Make Dialog modal (Dialog grabs all focus, master is not active)
-        self.protocol("WM_DELETE_WINDOW", self.cancel)  # Make "X" same as [Cancel] button, cancel is callback function
-
-        self._master = master           # save master window for this Dialog instance
-        #base class creates a result variable
-        self.result = None              # result is *public* data that can be accessed outside the class.
-                                        # result has the user input that is the result of the dialog with the user
-                                        # a way to send data bac by putting it in a public variable
-                                        # so you can do print(myWindow.result) - event driven
-                                        
-        if title:
-            self.title(title)           # Q2. What does this if statement do? 
-
-        self.v = tk.StringVar()         # Provide a generic StringVar v that can be used to store user input data for the transaction
-        self.v.set('ERROR: uninitialized data')    # if a derived class wants to use v, it must set v
-        
-=======
         self.grab_set()                                 # Make Dialog modal (Dialog grabs all focus, master is not active)
         self.protocol("WM_DELETE_WINDOW", self.cancel)  # Make "X" same as [Cancel] button, cancel is callback function
 
@@ -80,20 +37,10 @@ class Dialog(tk.Toplevel, ABC):    # Multiple inheritance: Dialog gets the attri
         self.v = tk.StringVar()         # Provide a generic StringVar v that can be used to store user input data for the transaction
         self.v.set('ERROR: uninitialized data')    # if a derived class wants to use v, it must set v
 
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
         bodyFrame = tk.Frame(self)                      # Create empty body frame for derived class to fill
         self.initial_focus = self.body(bodyFrame)       # Call the body() method to populate the window's body.
                                                 # The body method will return a widget, and the focus will be on the returned widget.
                                                 # Having a focus on a widget means the cursor will be at that widget.
-<<<<<<< HEAD
-                                                #             
-        bodyFrame.pack(padx=5, pady=5, fill=tk.BOTH, expand='y')  
-
-        self.buttonbox()                        # create [OK] and [Cancel] buttons as another frame
-                                                # a box of two buttons
-        if not self.initial_focus:              # if focus is not on a widget, then focus is Dialog 
-            self.initial_focus = self           # 
-=======
                                                            
         bodyFrame.pack(padx=5, pady=5, fill=tk.BOTH, expand='y')  
 
@@ -101,47 +48,23 @@ class Dialog(tk.Toplevel, ABC):    # Multiple inheritance: Dialog gets the attri
 
         if not self.initial_focus:              # if focus is not on a widget, then focus is Dialog 
             self.initial_focus = self
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
         self.initial_focus.focus_set()          # set the focus
         
         # Q3. Explain where the focus could be. There are 3 possibilities, with a certain precedence: first, second, third
         # List the 3 locations in order.
-<<<<<<< HEAD
-        # first:
-        # second:
-        # third: 
-
-=======
         # first : mainWindow
         # second: dialog (prompting user to enter ID, name, language)
         # third: if the langue is not python, prompt message
         
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
         self.transient(master)      # Set Dialog to be transient to the master:
                                     # This means: 1. Dialog will minimize if master is minimized 
                                     # 2. Dialog causes no extra icon on taskbar
                                     # 3. Dialog appears on top of master
-<<<<<<< HEAD
-                                    
-                                    
-                                    #string format!!
-=======
 
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
         self.geometry("+%d+%d" % (master.winfo_rootx()+50, master.winfo_rooty()+50)) # place Dialog window on right and down from master
         self.resizable(False,False) # Don't allow Dialog to be sizeable
 
         self.wait_window(self)      # Stay open until Dialog is closed by the user
-<<<<<<< HEAD
-    #
-    #=====  methods for appearance and behavior of Dialog  =====
-    # 
-    
-    @abstractmethod #Decorator, if anybody tries to instantiates the dialog class by itself, they will get error message
-    def body(self, bodyFrame):
-        """Create dialog body.  Return widget that should have initial focus."""
-
-=======
             
             
     #
@@ -150,20 +73,13 @@ class Dialog(tk.Toplevel, ABC):    # Multiple inheritance: Dialog gets the attri
     @abstractmethod
     def body(self, bodyFrame):
         """Create dialog body.  Return widget that should have initial focus."""
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
         raise NotImplementedError
 
     def buttonbox(self):
         """Add [Ok] and [Cancel] buttons]"""
-<<<<<<< HEAD
-        box = tk.Frame(self) # add the frame
-
-        self.b_ok = tk.Button(box, text="OK", width=10, command=self.ok) #callback
-=======
         box = tk.Frame(self)
 
         self.b_ok = tk.Button(box, text="OK", width=10, command=self.ok) 
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
         self.b_ok.pack(side=tk.LEFT, padx=5, pady=5)
         self.b_cancel = tk.Button(box, text="Cancel", width=10, command=self.cancel)
         self.b_cancel.pack(side=tk.LEFT, padx=5, pady=5)
@@ -182,28 +98,17 @@ class Dialog(tk.Toplevel, ABC):    # Multiple inheritance: Dialog gets the attri
         raise NotImplementedError        
 
     def ok(self, *args):
-<<<<<<< HEAD
-        """[Ok] button to commit change"""  
-        if not self.validate():                     # if not valid
-            self.initial_focus.focus_set()          # put focus back to initial focus
-            return #if it doesnt return true, the ok cannot be done
-=======
         """[Ok] button to commit change"""
         if not self.validate():                     # if not valid
             self.initial_focus.focus_set()          # put focus back to initial focus
             return
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
 
         self.apply()                # if everything is valid, then store input data into result
         self.cancel()               # go to close window
 
     def cancel(self, *args):
         """[Cancel] button to close window"""
-<<<<<<< HEAD
-        self._master.focus_set()    # set focus back to the master window, close top level window
-=======
         self._master.focus_set()    # set focus back to the master window
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
         self.destroy()              # close window
 
     def return_(self, *args):
@@ -219,18 +124,6 @@ class Dialog(tk.Toplevel, ABC):    # Multiple inheritance: Dialog gets the attri
                                      # If derived class handles multiple data in a data structure
                                      # then the derive class should override this method so result
                                      # can be a data structure.
-<<<<<<< HEAD
-        # v is from the random variable, need to get a string from a user
-        # need to write this
-        # need to end this with self.result = something
-        
-        
-# Q1. what is kwargs and why would this base class want to use it        
-# kwargs makes the baseclass accept any arbitrary number of arguments since we do not know how many
-# arguments there are in the derived class.
-# Q4. Name all the callback methods
-=======
 
 # Q4. Name all the callback methods
 # body, buttonbox, validate (definition in inheritance is enfonrced), ok, cancel, return_, apply, 
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
--- lab3.py
+++ lab3.py
@@ -1,75 +1,3 @@
-<<<<<<< HEAD
-# lab 3: GUI with Tkinter, OOP
-# Name: Mega Putra 
-import tkinter as tk
-import tkinter.messagebox as tkmb #for message box
-import platform
-import os
-from dialog import *
-
-class AddStudentDialog(dialog) :
-        '''  
-        def __init__(self, master, title, **kwargs):
-        super(AddStudentDialog, self).__init__()  
-        '''
-        '''
-        self._stuID_entry = tk.StringVar()
-        self._stuID_entry = tk.StringVar()
-        self._favLang_entry = tk.StringVar()
-        self.body()
-        '''
-        
-        def body(self, frame):
-        # The body method will return a widget, and the focus will be on the returned widget.
-        # Having a focus on a widget means the cursor will be at that widget.        
-                self.title("Student Info")
-                
-                #create 3 labels
-                stuID = tk.Label(self, text="Student ID")
-                name = tk.Label(self, text = "Name")
-                favLang = tk.Label(self, text = "Favorite Language")
-                stuID.grid(row = 0, column = 0, sticky='e')
-                name.grid(row = 1, column = 0, sticky='e')
-                favLang.grid(row = 2, column = 0, sticky='e')
-                
-                #create entry boxes for the labels
-                tk.Entry(self, textvariable = self._stuID_entry).grid(row = 0, column = 1)  
-                tk.Entry(self, textvariable = self._name_entry).grid(row = 1, column = 1)
-                tk.Entry(self, textvariable = self._favLang_entry).grid(row = 2, column = 1)   
-                
-                #create [OK] and [Cancel] buttons
-                def okButton() : print("OK")         
-                okButton = tk.Button(self, text="OK", command = okButton)
-                okButton.grid(row = 4, column = 0)            
-                
-                def cancelButton() : print("Cancel") 
-                cancelButton = tk.Button (self, text = "Cancel", command = cancelButton)
-                cancelButton.grid(row = 4, column = 1)
-    
-    def validate(self): 
-        if(self._stuID_entry != "" and self._stuID_entry.isdigit() and len(self._stuID_entry) == 3): # check studentID entry
-            if self._favLang_entry != "Python" or self._favLang_entry != "python" :
-                #prints mesage box
-                tkmb.showinfo("Notification", "Too bad it's not Python!", parent = self)
-            return True
-        else:
-            return False
-                
-        
-    #def apply(self): #overriding the base class method   
-        
-        
-# class MainWindow: 
- 
-def main() :
-    win = AddStudentDialog()
-    if platform.system() == 'Darwin': 
-        tmpl = 'tell application "System Events" to set frontmost of every process whose unix id is %d to true'
-        os.system("/usr/bin/osascript -e '%s'" % (tmpl % os.getpid()))    
-    win.mainloop()
-
-main()
-=======
 # lab 3: GUI with Tkinter, OOP
 import os 
 import platform
@@ -77,92 +5,91 @@ from dialog import *
 
 # class AddStudentDialog
 class AddStudentDialog(Dialog):
-    
-    '''
+
+        '''
     def __init__(self, title = None, **kwargs):
         super().__init__(title, **kwargs)
     '''
-    def body(self, bodyFrame):
-        print("call body")
-        IDLabel = tk.Label(bodyFrame, text = "Student ID: ")
-        IDLabel.grid(row = 0, column = 0)
-        ID = tk.StringVar()
-        IDEntry = tk.Entry(bodyFrame, textvariable = ID) 
-        IDEntry.grid(row = 0, column = 1)
-        
-        nameLabel = tk.Label(bodyFrame, text = "Name: ")
-        nameLabel.grid(row = 1, column = 0)
-        name = tk.StringVar()
-        nameEntry = tk.Entry(bodyFrame, textvariable = name) 
-        nameEntry.grid(row = 1, column = 1) 
-        
-        languageLabel = tk.Label(bodyFrame, text = "Favorite Languge: ")
-        languageLabel.grid(row = 2, column = 0)
-        language = tk.StringVar()
-        languageEntry = tk.Entry(bodyFrame, textvariable = language) 
-        languageEntry.grid(row = 2, column = 1)           
-    
-    def validate(self):
-        print("call validate")
-        pass
-    
-    def apply(self):
-        print("call apply")
-        pass
-    
- 
+        def body(self, bodyFrame):
+                print("call body")
+                IDLabel = tk.Label(bodyFrame, text = "Student ID: ")
+                IDLabel.grid(row = 0, column = 0)
+                ID = tk.StringVar()
+                IDEntry = tk.Entry(bodyFrame, textvariable = ID) 
+                IDEntry.grid(row = 0, column = 1)
+
+                nameLabel = tk.Label(bodyFrame, text = "Name: ")
+                nameLabel.grid(row = 1, column = 0)
+                name = tk.StringVar()
+                nameEntry = tk.Entry(bodyFrame, textvariable = name) 
+                nameEntry.grid(row = 1, column = 1) 
+
+                languageLabel = tk.Label(bodyFrame, text = "Favorite Languge: ")
+                languageLabel.grid(row = 2, column = 0)
+                language = tk.StringVar()
+                languageEntry = tk.Entry(bodyFrame, textvariable = language) 
+                languageEntry.grid(row = 2, column = 1)           
+
+        def validate(self):
+                print("call validate")
+                pass
+
+        def apply(self):
+                print("call apply")
+                pass
 
 
- 
+
+
+
 
 # class MainWindow
- 
+
 class MainWindow(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Lab3")              # set title lab3
-        self.resizable(True, False)     # set resizability horizontal only
-        
-        # create Add Label
-        addLabel = tk.Label(self, text = "Add a Student")
-        addLabel.grid()
-  
-  
-  
-        # create Add Button
-        def addStudent():
-            prompt = AddStudentDialog(self)        
-        addButton = tk.Button(self, text = "Click to Add", command = addStudent)
-        addButton.grid(row = 0, column = 1)
-          
-        # create Student List Label
-        StudentListLabel = tk.Label(self, text = "Student List")
-        StudentListLabel.grid(row = 0, column = 2)        
-        # create the scroll bar
-        scrollBar = tk.Scrollbar(self)
-        listBox = tk.Listbox(self, height = 3, yscrollcommand = scrollBar.set)
-        listBox.grid(row = 1, column = 2, sticky = "ew")
-        scrollBar.config(command = listBox.yview)
-        scrollBar.grid(row = 1, column = 3)
-        # set expansion for column 2
-        self.columnconfigure(2, weight = 1)
-        
+        def __init__(self):
+                super().__init__()
+                self.title("Lab3")              # set title lab3
+                self.resizable(True, False)     # set resizability horizontal only
 
-        
-        
-        #
-        countLabel = tk.Label(self, text = "Student Count: ")
-        countLabel.grid(row = 1, column = 0)        
+                # create Add Label
+                addLabel = tk.Label(self, text = "Add a Student")
+                addLabel.grid()
 
-        
+
+
+                # create Add Button
+                def addStudent():
+                        prompt = AddStudentDialog(self)        
+                addButton = tk.Button(self, text = "Click to Add", command = addStudent)
+                addButton.grid(row = 0, column = 1)
+
+                # create Student List Label
+                StudentListLabel = tk.Label(self, text = "Student List")
+                StudentListLabel.grid(row = 0, column = 2)        
+                # create the scroll bar
+                scrollBar = tk.Scrollbar(self)
+                listBox = tk.Listbox(self, height = 3, yscrollcommand = scrollBar.set)
+                listBox.grid(row = 1, column = 2, sticky = "ew")
+                scrollBar.config(command = listBox.yview)
+                scrollBar.grid(row = 1, column = 3)
+                # set expansion for column 2
+                self.columnconfigure(2, weight = 1)
+
+
+
+
+                #
+                countLabel = tk.Label(self, text = "Student Count: ")
+                countLabel.grid(row = 1, column = 0)        
+
+
 def main() :
-    win = MainWindow()
-    
-    if platform.system() == 'Darwin': 
-        tmpl = 'tell application "System Events" to set frontmost of every process whose unix id is %d to true'
-        os.system("/usr/bin/osascript -e '%s'" % (tmpl % os.getpid()))
-        
-    win.mainloop()
+        win = MainWindow()
+
+        if platform.system() == 'Darwin': 
+                tmpl = 'tell application "System Events" to set frontmost of every process whose unix id is %d to true'
+                os.system("/usr/bin/osascript -e '%s'" % (tmpl % os.getpid()))
+
+        win.mainloop()
 
 main()
->>>>>>> a79718234f7fdfd05ccdf78fc1f5bafc88a4b399
